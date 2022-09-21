558af8a18ad2ff0f4bc51c54b322f2618df9bf07
Lauren <l.cvengros@icloud.com>
2022-09-21 15:07:18 -0400

    created tests for engineer

---
+++ _tests_/Engineer_test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+    const github = "githubtestaccount";
+    const employee = new Engineer("Lauren", "1234", "test@email.com", github);
+    expect(employee.github).toBe(github);
+});
+
+test("Get gitHub", () => {
+    const testGithub = "githubtestaccount";
+    const employee = new Engineer("Lauren", "1234", "test@email.com", testGithub);
+    expect(employee.getGitHub()).toBe(testGithub);
+})
+
+test("getRole function", () => {
+    const returnEngineer = "Engineer";
+    const employee = new Engineer("Lauren", "1234", "test@email.com", returnEngineer);
+    expect(employee.getRole()).toBe(returnEngineer);
+})
+
